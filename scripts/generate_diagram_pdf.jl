f142396ffdebdabd96d63d83fa29f84888552342
zhaoli-IHEP <zhaoli@ihep.ac.cn>
2023-02-24 22:31:06 +0800

    change to use endswith in generate_diagram_pdf.jl

--- scripts/generate_diagram_pdf.jl
+++ scripts/generate_diagram_pdf.jl
@@ -1,9 +1,9 @@
 root, dirs, files = (first∘collect∘walkdir)(".")
 
-tex_list = filter( s->s[end-2:end] == "tex", files )
+tex_list = filter( s->endswith(s,".tex"), files )
 tex_head_list = map( s->s[1:end-4], tex_list )
 
-pdf_list = filter( s->s[end-2:end] == "pdf", files )
+pdf_list = filter( s->endswith(s,".pdf"), files )
 pdf_head_list = map( s->s[1:end-4], pdf_list )
 
 mission_head_list = setdiff( tex_head_list, pdf_head_list )
